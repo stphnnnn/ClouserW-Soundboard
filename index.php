f02403d7f1e4536ace669bce0c0f34b826ce6f88
Steve Burtenshaw <steveburtenshaw@Steves-MacBook-Pro.local>
2016-11-30 20:59:03 +0000

    Edits

--- index.php
+++ index.php
@@ -1,56 +1,29 @@
-<?php
-    $files = glob("clips/*.ogg")
-?>
+<?php $files = glob("sounds/*.{mp3, ogg, wav}", GLOB_BRACE); ?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>ClouserW Soundboard</title>
-	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css">
-	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/base/base-min.css"> 
-	<style type="text/css" media="screen">
-	   button {
-	       -moz-border-radius: 5px;
-	       background: red;
-	       border: 3px solid black;
-	       display: inline-block;
-	       cursor: pointer;
-	       padding: 3px 5px;
-	       margin: 5px;
-	   }
-	   
-	   html {
-	       background: url(http://farm4.static.flickr.com/3112/2721341322_e73bca6a9e_b.jpg);
-	   }
-	   
-	   h1 {
-	       color: white;
-	   }
-	</style>
-	
-	
+	<title>soundboard</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="doc">
-        <h1>ClouserW Soundboard</h1>
-        <!-- JS here to prevent 'flash' of all the default audio players -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-        <script type="text/javascript" charset="utf-8">
-            $(function() {
-                $("audio").removeAttr("controls").each(function(i, audioElement) {
-                    var audio = $(this);
-                    var that = this; //closure to keep reference to current audio tag
-                    $("#doc").append($('<button>'+audio.attr("title")+'</button>').click(function() {
-                        that.play();
-                    }));
-                });
-            });
-        </script>
-        <?php foreach($files as $file) { ?>
-            <?php $title = str_replace(".ogg", "", str_replace("clips/", "", $file)); ?>
-            <audio src="<?php echo $file; ?>" controls autobuffer="true" title="<?php echo $title ?>"></audio>
-        <?php } ?>
-    </div>
-    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script type="text/javascript" charset="utf-8">
+    $(function() {
+      $("audio").removeAttr("controls").each(function(i, audioElement) {
+        var audio = $(this);
+        var that = this;
+        $(".players").append($('<button>'+audio.attr("title")+'</button>').click(function() {
+          that.play();
+        }));
+      });
+    });
+  </script>
+  <div class="players">
+    <?php foreach($files as $file) { ?>
+      <?php $title = str_replace(".ogg", "", str_replace("sounds/", "", $file)); ?>
+      <audio src="<?php echo $file; ?>" controls autobuffer="true" title="<?php echo $title ?>"></audio>
+    <?php } ?>
+  </div>
 </body>
 </html>
